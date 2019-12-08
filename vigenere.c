10767e87ee959481436dc9b69c2ad6eb453d6a0c
Xiaocong(Bill) Yan <50472308+Xiaocong233@users.noreply.github.com>
2019-12-08 10:16:38 -0800

    Update vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -5,7 +5,7 @@
 #include <ctype.h>
 
 int shift(char c);
-void CaesarCipher(int Key, string text, int Pos);
+void RotationalCipher(int Key, string text, int Pos);
 
 int main(int argc, string argv[])
 {
@@ -15,7 +15,7 @@ int main(int argc, string argv[])
         bool check = false;
         int keyLength = strlen(userInput);
         int restoration = keyLength;
-        for (int i = 0; i < keyLength; i++) //check if all characters in the second string input are all alphabetical
+        for (int i = 0; i < keyLength; i++) //check if all characters in the second string input that is the key are all alphabetical
         {
             if (userInput[i] >= 'A' && userInput[i] <= 'z')
             {
@@ -34,20 +34,20 @@ int main(int argc, string argv[])
             int n = 0;
             int numOfSpace = 0;
             printf("ciphertext: ");
-            for (int i = 0; i < strlen(plaintext); i++)
+            for (int i = 0; i < strlen(plaintext); i++) //encrypting every characters of the text with the corresponding key character
             {
-                if (plaintext[i] == ' ' || plaintext[i] == ',' || plaintext[i] == '.' || plaintext[i] == '!' || plaintext[i] == '?' || plaintext[i] == '\"' || plaintext[i] == '\'')
+                if (plaintext[i] == ' ' || plaintext[i] == ',' || plaintext[i] == '.' || plaintext[i] == '!' || plaintext[i] == '?' || plaintext[i] == '\"' || plaintext[i] == '\'') //nullify the rotation of the key if the character being encrypted are any of the special characters
                 {
                     numOfSpace++;
                     n--;
                 }
-                if (i == (keyLength + numOfSpace))
+                if (i == (keyLength + numOfSpace)) //restore the encrypting key character to the first character if reaches the end of the key
                 {
                     n = 0;
                     keyLength += restoration;
                 }
-                key = shift(argv[1][n]); //assign key as an integer value converted from user's string input
-                CaesarCipher(key, plaintext, i); //execute cipher function
+                key = shift(argv[1][n]); //assign key as an integer value converted from the current letter of the user's string input
+                RotationalCipher(key, plaintext, i); //execute rotational cipher function
                 n++;
             }
             printf("\n");
@@ -66,24 +66,24 @@ int main(int argc, string argv[])
     }
 }
 
-int shift(char c)
+int shift(char c) //return the number of alphabetic shift needed from the character of the key to encrypt the message
 {
     int shiftNumber = toupper(c) - 65;
     return shiftNumber;
 }
 
-void CaesarCipher(int Key, string text, int Pos) //prints the ciphered text based on the rotation of the characters in user's text input alphabetically according to user's input of a key that marks the number of rotation
+void RotationalCipher(int Key, string text, int Pos) //prints the ciphered text based on the rotation of the characters in user's text input alphabetically according to user's input of a key that marks the number of rotation
 {
-    if (text[Pos] >= 'A' && text[Pos] <= 'Z') //check if the single character in the loop belongs to the cap'd group
+    if (text[Pos] >= 'A' && text[Pos] <= 'Z') //check if the character is captilized
     {
         printf("%c", (65 + ((text[Pos] - 65 + Key) % 26))); //calculate and print the new character
     }
-    else if (text[Pos] >= 'a' && text[Pos] <= 'z') //check if the single character in the loop belongs to the lowercased group
+    else if (text[Pos] >= 'a' && text[Pos] <= 'z') //check if the character is lowercased
     {
-        printf("%c", (97 + ((text[Pos] - 97 + Key) % 26)));
+        printf("%c", (97 + ((text[Pos] - 97 + Key) % 26))); //calculate and print the new character
     }
     else
     {
-        printf("%c", text[Pos]);
+        printf("%c", text[Pos]); //print the character as it is
     }
 }
